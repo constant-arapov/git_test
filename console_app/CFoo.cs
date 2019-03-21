1784c653dc21513a0892dd46c5dc04ddd2dcb422
constant-arapov <constant_arapov@bk.ru>
2019-03-21 07:36:19 +0500

    |Added Foo class

---
+++ console_app/CFoo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app
+{
+    public static class CFoo
+    {
+        public static void Do()
+        {
+            Console.WriteLine("Do Job !");
+        }
+
+
+
+    }
+}
