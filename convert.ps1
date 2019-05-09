4ff820830a063f66f9c44fb72e3e45da5b452219
constant-arapov <31322617+constant-arapov@users.noreply.github.com>
2019-05-09 14:50:38 +0500

    Update convert.ps1

--- convert.ps1
+++ convert.ps1
@@ -1,2 +1,2 @@
-add-appxpackage -path Microsoft.DesktopAppConverter_2.1.4.0_x64__8wekyb3d8bbwe.Appx
+add-appxpackage -path Microsoft.DesktopAppConverter_2.1.4.0_x64__8wekyb3d8bbwe.Appx 
 DesktopAppConverter.exe -Installer MyApplication.msi -Destination Appx05 -PackageName "MyCompany.MyApplication"  -Publisher "CN=12345678-1234-1234-1234-123456789012" -PackagePublisherDisplayName "FileStar AB" -PackageDisplayName  "FileStar" -Version 1.0.14.0 -MakeAppx -Verbose
